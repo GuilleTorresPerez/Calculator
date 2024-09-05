548c4f0c6d4d217fbbadce82febd8c503f0c265a
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-09-05 13:01:21 +0200

    Finish calculator logic

--- script.js
+++ script.js
@@ -1,30 +1,91 @@
 let firstOperand = 0;
 let secondOperand = 0;
-let operator = 0;
+let operator = '';
+let result = 0;
+const display = document.querySelector('.display');
+const calculatorSM = new operationStateMachine();
 
 class operationStateMachine {
     constructor() {
-        this.state = 'writing first number';
+        this.state = 'init state';
     }
 
     digitPressed(digit) {
-        console.log("actual state: " + this.state);
-
-        if(this.state == 'writing first number') {
-            firstOperand = (firstOperand * 10) + digit;
-            display.innerHTML += digit;
-
-            console.log("the first number now is: " + firstOperand);
+        switch (this.state) {
+            case 'init state':
+                display.innerHTML = '';
+                clearOperation();
+            case 'writing first number':
+                firstOperand = (firstOperand * 10) + digit;
+                display.innerHTML += digit;
+                this.state = 'writing first number';  
+                console.log("The first number now is: " + firstOperand);
+                break;
+            case 'writing second number':
+                secondOperand = (secondOperand * 10) + digit;
+                display.innerHTML += digit;
+                this.state = 'writing second number';
+                console.log("The second number now is: " + secondOperand);
+                break;
         }
+    }
 
-        if(this.state == 'writing second number') {
-            secondOperand = (secondOperand * 10) + digit;
-            display.innerHTML += digit;
+    operatorPressed(char) {
+        console.log("Operator pressed");
 
-            console.log("the second number now is: " + secondOperand);            
+        switch (this.state) {
+            case 'init state':
+                firstOperand = result;
+                secondOperand = 0;
+                result = 0;
+                operator = char;
+                this.state = 'writing second number';
+                display.innerHTML += ' ' + char + ' ';
+                break;
+            case 'writing second number':
+                // Do nothing
+                break;
+
+            case 'writing first number':
+                operator = char;
+                this.state = 'writing second number';
+                display.innerHTML += ' ' + char + ' ';
+                break;
+        }
+    }
+
+    equalPressed() {
+        switch (this.state) {
+            case 'init state':
+            case 'writing first number':
+                // Do nothing
+                break;
+
+            case 'writing second number':
+                console.log("The first number now is: " + firstOperand);
+                console.log("The operator now is: " + operator);
+                console.log("The second number now is: " + secondOperand);
+
+                result = operate(firstOperand, secondOperand, operator);
+                display.innerHTML = result;
+                this.state = 'init state';
+                break;
+        }
+    }
+
+    clearPressed() {
+        switch (this.state) {
+            case 'init state':
+            case 'writing first number':
+            case 'writing second number':
+                display.innerHTML = '';
+                this.state = 'init state';
+                clearOperation();
+                break;
         }
     }
 }
+
 
 const add = (num1, num2) => num1 + num2;
 const substract = (num1, num2) => num1 - num2;
@@ -44,10 +105,15 @@ function operate(num1, num2, operator) {
     }
 }
 
-const display = document.querySelector('.display');
-const calculatorSM = new operationStateMachine();
+const clearOperation = () => {
+    firstOperand = 0; 
+    secondOperand = 0; 
+    operator = ''; 
+    result = 0;
+};
 
-function digitPressed(digit) {
-    calculatorSM.digitPressed(digit);
-}
+const digitPressed = (digit) => calculatorSM.digitPressed(digit);
+const operatorPressed = (char) => calculatorSM.operatorPressed(char); 
+const equalPressed = () => calculatorSM.equalPressed();
+const clearPressed = () => calculatorSM.clearPressed();
 
